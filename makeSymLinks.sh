84b55262a4f6bb64fd367ff8c67a284982df19c7
Tatsuhiko Nakamine <nakamine@ip-10-3-4-82.ap-northeast-1.compute.internal>
2018-09-05 17:43:24 +0900

    gitignoreとgitconfigをgitディレクトリに移動

--- makeSymLinks.sh
+++ makeSymLinks.sh
@@ -3,8 +3,8 @@
 # make symbolic links
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/gitignore ~/.gitignore
+ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
